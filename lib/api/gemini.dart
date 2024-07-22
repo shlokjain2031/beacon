3143ba8204bc9fa9d95b025e5f6579e4d52ab540
Shlok Jain <jainshlok20@gmail.com>
2024-07-22 17:49:09 +0530

    draft edge function and POST req set up

--- lib/api/gemini.dart
+++ lib/api/gemini.dart
@@ -1,23 +1,32 @@
-import 'package:beacon/model/portfolio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class GeminiApi {
   final supabase = Supabase.instance.client;
 
-  Future<double> getEssayRating(Portfolio portfolio) async {
-    return await supabase.functions.invoke(
+  Future<String> getEssayRating(String essayPrmopt) async {
+
+    await dotenv.load(fileName: "SUPABASE_API_KEY.env");
+
+    String essayRating = await supabase.functions.invoke(
       'gemini-api',
       headers: {
-        "Authorization": "ACCESS_TOKEN",
+        "Authorization": "Bearer ${dotenv.env['SUPABASE_API_KEY'] as String}",
         "content-type": "application/json"
       },
       body: {
-        // todo: include the essay
+        "essayPrompt" : essayPrmopt
       },
     ).then((response) {
       return response.data;
+    }).catchError((e) {
+      print(e);
     });
+
+    print(essayRating);
+
+    return essayRating;
   }
 
-  // todo: safety reach list
+// todo: safety reach list
 }
--- supabase/functions/gemini-api/essay_prompt.ts
+++ supabase/functions/gemini-api/essay_prompt.ts
@@ -1,11 +1,23 @@
 import {GoogleGenerativeAI} from "https://cdn.jsdelivr.net/npm/@google/generative-ai/+esm";
+import * as dotenv from 'dotenv';
 
 console.log("Hello from Functions!");
 
+// deno-lint-ignore no-var
+declare var process : {
+  env: {
+    GEMINI_API_KEY: "AIzaSyDcrVA7mDq4EKZSccbEEEWXaO8SpLjlnog",
+  }
+}
+
 Deno.serve( (req) => {
 
+  dotenv.config();
+
+  console.log("hello");
+
   // todo: fetch your API_KEY from env file
-  const API_KEY = "AIzaSyDcrVA7mDq4EKZSccbEEEWXaO8SpLjlnog";
+  const API_KEY = process.env.GEMINI_API_KEY;
 
   const genAI = new GoogleGenerativeAI(API_KEY);
 
@@ -15,11 +27,13 @@ Deno.serve( (req) => {
 
   async function run() {
 
-    // todo: get prompt structure from .env and then use req to define parameters
-    const prompt = ""
-  
-    const result = await model.generateContent(prompt);
+    const { essayPrompt } = await req.json()
+
+    const result = await model.generateContent(essayPrompt);
     const response = await result.response;
+
+    console.log(response);
+
     const text = response.text();
     console.log(text);
 
@@ -29,9 +43,8 @@ Deno.serve( (req) => {
   run();
 
   // todo: write the return as a string
-  return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json'} })
+  return new Response(data, { headers: { 'Content-Type': 'application/json'} })
 })
-
 
 
 /* To invoke locally:
@@ -39,6 +52,6 @@ Deno.serve( (req) => {
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/gemini-api' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
+    --data '{"essayPrompt":"Hello Bro"}'
 
 */
